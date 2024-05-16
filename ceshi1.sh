1fd5772bbcd5e5d2ec75d8cad2da900fa3ce1c4e
一页书 <96804858+pili-book@users.noreply.github.com>
2024-05-16 21:35:27 +0800

    Update ceshi1.sh

--- ceshi1.sh
+++ ceshi1.sh
@@ -1,4 +1,20 @@
 #！/bin/sh
+wx(){
+#将下面的webhook地址替换成你的企业微信机器人地址，$1为告警消息 $2为@人的手机号 $2可以为空
+cat > $0.msg << EOF
+curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d25d7339-9b15-4b17-80ae-2f546140c349' \
+   -H 'Content-Type: application/json' \
+   -d '
+   {
+        "msgtype": "text",
+        "text": {
+            "content": "$1",
+            "mentioned_mobile_list":["$2"]
+        }
+   }'
+EOF
+sh $0.msg && rm -rf $0.msg
+}
 a=$(tcping -x 2 mailla.eu.org 81)
 b=$(tcping -x 2 192.168.2.1 81)
 c="open"
