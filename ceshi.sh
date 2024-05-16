55b828f4a40423f1255c5d50c002271941739914
一页书 <96804858+pili-book@users.noreply.github.com>
2024-05-16 16:22:22 +0800

    Update ceshi.sh

--- ceshi.sh
+++ ceshi.sh
@@ -1,7 +1,2 @@
-sudo apt-get install -y tcptraceroute bc
-sudo wget  https://github.com/pili-book/tcppingweixin/blob/master/tcping
-sudo chmod +x tcping
-sudo mv tcping /usr/local/bin/tcping
-tcping  -x 5  mailla.eu.org 81
 a=tcping  -x 5  mailla.eu.org 81
- echo $a
+ echo "$a"
