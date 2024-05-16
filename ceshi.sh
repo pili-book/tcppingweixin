fe2970725e45c0e8429b2f85320babf828fe3cdf
一页书 <96804858+pili-book@users.noreply.github.com>
2024-05-16 10:41:48 +0800

    Add files via upload

---
+++ ceshi.sh
@@ -0,0 +1,9 @@
+if tcping  -x 5  mailla.eu.org 81&>> /dev/null ;then
+        echo -n " 123"
+        success
+        echo "324 "
+    else
+        echo -n "321"
+        failure
+        echo "244 "
+    fi
