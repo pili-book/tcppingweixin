5efcac044e0e668236be4b1117ff4990d5c08d2f
一页书 <96804858+pili-book@users.noreply.github.com>
2024-05-16 10:44:47 +0800

    Update ceshi.sh

--- ceshi.sh
+++ ceshi.sh
@@ -1,9 +1,11 @@
+
+#!/bin/bash
 if tcping  -x 5  mailla.eu.org 81&>> /dev/null ;then
         echo -n " 123"
         success
-        echo "324 "
-    else
+        echo "324 " 
+else
         echo -n "321"
         failure
         echo "244 "
-    fi
+fi
