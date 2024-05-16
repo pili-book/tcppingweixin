72451940b1ec2f578c7c87b3cd06abba46741b6b
一页书 <96804858+pili-book@users.noreply.github.com>
2024-05-16 19:05:52 +0800

    Update ceshi.sh

--- ceshi.sh
+++ ceshi.sh
@@ -1,8 +1,6 @@
 a=$(tcping -x 2 mailla.eu.org 81)
 b=$(tcping -x 2 192.168.2.1 81)
 if [[ "$a" != *"open"* ]]; then
-  echo "变量中不包含字符串 open"
-else
-  echo "变量中包含字符串 open"
+  echo "变量中不包含字符串open"
 fi
 echo $b
