1f6de9f43476a4e3a6abf02960b2a5124a2b05d2
一页书 <96804858+pili-book@users.noreply.github.com>
2024-05-16 20:14:26 +0800

    Update ceshi.sh

--- ceshi.sh
+++ ceshi.sh
@@ -2,10 +2,11 @@ a=$(tcping -x 2 mailla.eu.org 81)
 b=$(tcping -x 2 192.168.2.1 81)
 c="open"
 result=$(echo $a | grep "${c}")
+do
 if  [ $a == *$c* ] 
 then
     echo "包含"
 else
     echo "不包含"
 fi
-
+done
