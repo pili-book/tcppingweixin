925851face5d09159cca6163f1d425564df644f2
一页书 <96804858+pili-book@users.noreply.github.com>
2024-05-16 23:17:22 +0800

    Update ceshi1.sh

--- ceshi1.sh
+++ ceshi1.sh
@@ -18,10 +18,17 @@ sh $0.msg && rm -rf $0.msg
 a=$(tcping -x 2 mailla.eu.org 81)
 b=$(tcping -x 2 192.168.2.1 81)
 c="open"
-result=$(echo $a | grep "${c}")
+result=$(echo $b | grep "${c}")
 if [ "$result" != "" ]; then
 echo "包含"
 else
-wx "路由器断线了注意注意..路由器断线了注意注意.....路由器断线了注意注意...路由器断线了注意注意"
+sleep 180
+d=$(tcping -x 5 mailla.eu.org 81)
+result=$(echo $b | grep "${c}")
+ if [ "$result" != "" ]; then
+ echo "包含"
+ else
+ wx "路由器断线了注意注意..路由器断线了注意注意.....路由器断线了注意注意...路由器断线了注意注意"
+ fi
 fi
 
