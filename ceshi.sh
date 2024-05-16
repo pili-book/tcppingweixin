d60f159faf97d3aca9931e41fac48364c45d8969
一页书 <96804858+pili-book@users.noreply.github.com>
2024-05-16 10:49:49 +0800

    Update ceshi.sh

--- ceshi.sh
+++ ceshi.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env sh
-if [tcping  -x 5  mailla.eu.org 81&>> /dev/null] ;then
-        echo "324 " 
+#!/bin/sh
+
+# -x 判断$myPath是否存在并且是否具有可执行权限
+if [tcping  -x 5  mailla.eu.org 81&>> /dev/null];then
+      echo "324 " 
 else
         echo -n "321" 
         failure
         echo "244 "
 fi
+
