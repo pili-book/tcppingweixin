b8ba4dae5d927b5737cc9fb4cb9ab944b3d789e2
一页书 <96804858+pili-book@users.noreply.github.com>
2024-05-16 21:17:50 +0800

    Update ceshi1.sh

--- ceshi1.sh
+++ ceshi1.sh
@@ -3,7 +3,7 @@ a=$(tcping -x 2 mailla.eu.org 81)
 b=$(tcping -x 2 192.168.2.1 81)
 c="open"
 result=$(echo $a | grep "${c}")
-if [ 1==2 ]; then
+if [ "$result" != "" ]; then
 echo "包含"
 else
 echo "不包含"
