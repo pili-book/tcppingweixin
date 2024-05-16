83de6dd8c7952435c4712040238e17587a62b3c4
一页书 <96804858+pili-book@users.noreply.github.com>
2024-05-16 22:12:37 +0800

    Update ceshi1.sh

--- ceshi1.sh
+++ ceshi1.sh
@@ -18,10 +18,10 @@ sh $0.msg && rm -rf $0.msg
 a=$(tcping -x 2 mailla.eu.org 81)
 b=$(tcping -x 2 192.168.2.1 81)
 c="open"
-result=$(echo $b | grep "${c}")
+result=$(echo $a | grep "${c}")
 if [ "$result" != "" ]; then
 echo "包含"
 else
-wx "这是一条告警信息" "18888888888"
+wx "路由器断线了注意注意"
 fi
 
