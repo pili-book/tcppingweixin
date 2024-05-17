e3eef1f61c2112b6d845b7db90da83945bc7c7a4
一页书 <96804858+pili-book@users.noreply.github.com>
2024-05-17 23:47:57 +0800

    Update ceshi1.sh

--- ceshi1.sh
+++ ceshi1.sh
@@ -18,11 +18,10 @@ sh $0.msg && rm -rf $0.msg
 a=$(tcping -x 2 mailla.eu.org 81)
 b=$(tcping -x 2 192.168.2.1 81)
 c="open"
-result=$(echo $b | grep "${c}")
+result=$(echo $a | grep "${c}")
 if [ "$result" != "" ]; then
 echo "包含"
 else
-wx "第一次路由器断线了注意注意..路由器断线了注意注意.....路由器断线了注意注意...路由器断线了注意注意"
 sleep 600
 d=$(tcping -x 5 mailla.eu.org 81)
 result=$(echo $d | grep "${c}")
